5ec560293964e00d5377b9a4437889573b603427
Daya3611 <dayanand.gawade@vsit.edu.in>
2024-09-21 19:53:27 +0530

    update fix2222

--- src/pages/video/[categoryId]/[videoId].js
+++
@@ -1,6 +0,0 @@
-import VideoDetails from "@/components/VideoDetails";
-
-export default function VideoDetailsPage({ params }) {
-  const { categoryId, videoId } = params;
-  return <VideoDetails categoryId={categoryId} videoId={videoId} />;
-}
